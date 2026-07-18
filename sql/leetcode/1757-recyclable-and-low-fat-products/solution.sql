407cfefd017e35895a47939ac4d7e3d436883b47
Shubham Kothari <shubhampk37@gmail.com>
2026-07-18 23:36:16 +0530

    Fix: Day 1 SQL code formatting

--- sql/leetcode/1757-recyclable-and-low-fat-products/solution.sql
+++ sql/leetcode/1757-recyclable-and-low-fat-products/solution.sql
@@ -1,7 +1,10 @@
 -- LeetCode 1757 Recyclable and Low Fat Products
 -- Problem Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
 
-SELECT product_id 
-FROM Products 
-WHERE low_fats = 'Y' 
+SELECT
+  product_id 
+FROM
+  Products 
+WHERE
+  low_fats = 'Y' 
   AND recyclable = 'Y';
