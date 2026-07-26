87bd24efee32f2fd5227cfc888af62bad6c887ef
Shubham Kothari <shubhampk37@gmail.com>
2026-07-26 21:52:25 +0530

    feat: add Day 10 SQL and Python practice solutions

---
+++ python/hackerrank/Finding-the-percentage/solution.py
@@ -0,0 +1,16 @@
+# HackerRank: Finding the percentage
+# Problem Link: https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+        
+    query_name = input()
+    
+    average_marks = sum(student_marks[query_name]) / len(student_marks[query_name]) 
+    
+    print(f"{average_marks:.2f}")
---
+++ sql/leetcode/620-Not-Boring-Movies/solution.sql
@@ -0,0 +1,15 @@
+-- 620. Not Boring Movies
+-- Problem Link: https://leetcode.com/problems/not-boring-movies
+
+SELECT
+    id,
+    movie,
+    description,
+    rating
+FROM 
+    Cinema
+WHERE
+    id % 2 != 0
+    AND description != 'boring'
+ORDER BY
+    rating DESC;
