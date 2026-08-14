f10b4d8f28ea919f4827e4279f5ca4bc944998aa
Shubham Kothari <shubhampk37@gmail.com>
2026-08-14 19:42:27 +0530

    refactor: optimize Day 26 SQL solution

--- sql/leetcode/1789-Primary-Department-for-Each-Employee/solution.sql
+++ sql/leetcode/1789-Primary-Department-for-Each-Employee/solution.sql
@@ -1,22 +1,47 @@
 -- LeetCode: 1789. Primary Department for Each Employee
 -- Problem Link: https://leetcode.com/problems/primary-department-for-each-employee
 
+-- Optimized code using Window Functions
+-- Much faster
+-- each employee's total department count in a single pass without needing a UNION
+
 SELECT
     employee_id,
     department_id
-FROM
-    Employee
+FROM 
+    (
+        SELECT
+            employee_id,
+            department_id,
+            primary_flag,
+            COUNT(*) OVER (PARTITION BY employee_id) AS total_depts
+        FROM
+            Employee
+    ) AS EmployeeDepartmentsCount
 WHERE
     primary_flag = 'Y'
+    OR total_depts = 1;
 
-UNION
 
-SELECT
-    employee_id,
-    department_id
-FROM
-    Employee
-GROUP BY
-    employee_id
-HAVING
-    COUNT(department_id) = 1;
+
+-- Previous Solution: UNION can degrade performance on larger datasets
+
+-- SELECT
+--     employee_id,
+--     department_id
+-- FROM
+--     Employee
+-- WHERE
+--     primary_flag = 'Y'
+
+-- UNION
+
+-- SELECT
+--     employee_id,
+--     department_id
+-- FROM
+--     Employee
+-- GROUP BY
+--     employee_id
+-- HAVING
+--     COUNT(department_id) = 1;
