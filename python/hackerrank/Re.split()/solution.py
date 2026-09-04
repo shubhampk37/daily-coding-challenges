dd9743a2b93d60710392b1ef427826d84c56298a
Shubham Kothari <shubhampk37@gmail.com>
2026-09-04 21:00:07 +0530

    feat: Add Day 50 SQL and Python practice solutions

---
+++ python/hackerrank/Re.split()/solution.py
@@ -0,0 +1,7 @@
+# HackerRank: Re.split()
+# Problem Link: https://www.hackerrank.com/challenges/re-split/problem
+
+regex_pattern = r"[,.]+"
+
+import re
+print("\n".join(re.split(regex_pattern, input())))
